d5afd1606aaa93dc143920017e4872c0d7bd3ca8
ricoseeds <rghchaks73@gmail.com>
2018-11-21 15:05:43 +0530

    Intial setup

--- NET2d/main.cpp
+++ NET2d/main.cpp
@@ -1,15 +1,45 @@
-//
-//  main.cpp
-//  NET2d
-//
-//  Created by Argha Chakraborty on 21/11/18.
-//  Copyright © 2018 Argha Chakraborty. All rights reserved.
-//
-
 #include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "NET2D", nullptr, nullptr);
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    if (nullptr == window) {
+        cout << "Failed to create window";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (GLEW_OK != glewInit()) {
+        cout << "Failed to intitalize GLEW";
+        return -1;
+    }
+    glViewport(0, 0, screenWidth, screenHeight);
+    //game loop
+    
+    while ( !glfwWindowShouldClose( window )) {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
     return 0;
 }
