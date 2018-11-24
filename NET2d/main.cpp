29f2326e95ee09ddde01179994dc08990437a7ff
ricoseeds <rghchaks73@gmail.com>
2018-11-24 15:55:58 +0530

    Freehand with points

--- NET2d/main.cpp
+++ NET2d/main.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-
+vector<vector<double>> postions;
 static void cursorPositionCallback( GLFWwindow *window, double xpos, double ypos );
+
 void cursorEnterCallback( GLFWwindow *window, int entered );
 void mouseButtonCallback( GLFWwindow *window, int button, int action, int mods );
 void scrollCallback( GLFWwindow *window, double xoffset, double yoffset );
@@ -51,23 +52,25 @@ int main( void )
     glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
     glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1 ); // essentially set coordinate system
     glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
-    vector<vector<double>> postions;
     
     // Loop until the user closes the window
+    
     while ( !glfwWindowShouldClose( window ) )
     {
         glClear( GL_COLOR_BUFFER_BIT );
 
         double xpos, ypos;
         glfwGetCursorPos( window, &xpos, &ypos);
-        vector <double> tmppos;
-        tmppos.push_back(xpos);
-        tmppos.push_back(ypos);
-        postions.push_back(tmppos);
-        std::cout << tmppos[0] << " : " << tmppos[1] << std::endl;
+//        vector <double> tmppos;
+//        tmppos.push_back(xpos);
+//        tmppos.push_back(ypos);
+//        postions.push_back(tmppos);
+//        std::cout << tmppos[0] << " : " << tmppos[1] << std::endl;
+//
+        
         
         // Draw the guiding curve
-        drawPoint(xpos, ypos);
+//        drawPoint(xpos, ypos);
         
         // Draw Buggy Freehand sketch lol
         freeHandSketch(postions);
@@ -85,24 +88,26 @@ int main( void )
 }
 
 GLfloat adjustY(double yVal){
-    return (GLfloat)(-yVal+ SCREEN_HEIGHT);
+    return (GLfloat)(-yVal + SCREEN_HEIGHT);
 }
 
 void drawPoint(double xpos, double ypos){
     glEnable( GL_POINT_SMOOTH );
     glPointSize( 5 );
     glBegin(GL_POINTS);
-    glColor4f(1,0,0,1);
+    glColor3ub( 255, 0, 0 );
     glVertex2f((GLfloat)xpos, adjustY(ypos));
     glEnd();
 }
 
 void freeHandSketch(vector<vector<double>> positions){
     glEnable( GL_POINT_SMOOTH );
-    glBegin( GL_LINE_STRIP );
+//    glBegin( GL );
+    glBegin(GL_POINTS);
+    glPointSize( 5 );
+    glBegin( GL_POINT );
     glColor3ub( 255, 0, 0 );
-    for( size_t i = 0; i < positions.size(); i += 2 )
-    {
+    for( size_t i = 0; i < positions.size(); i += 1 ) {
         glVertex2f( (GLfloat)positions[i][0], adjustY(positions[i][1]));
     }
     glEnd();
@@ -110,30 +115,41 @@ void freeHandSketch(vector<vector<double>> positions){
 
 static void cursorPositionCallback( GLFWwindow *window, double xpos, double ypos )
 {
-    //std::cout << xpos << " : " << ypos << std::endl;
+    std::cout << xpos << " : " << ypos << std::endl;
+    // drag positions
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        cout<< "the actual fuck";
+        vector <double> tmppos;
+        tmppos.push_back(xpos);
+        tmppos.push_back(ypos);
+        postions.push_back(tmppos);
+    }
 }
 
 void cursorEnterCallback( GLFWwindow *window, int entered )
 {
-    if ( entered )
-    {
+    if ( entered ) {
         std::cout << "Entered Window" << std::endl;
-    }
-    else
-    {
+    } else {
         std::cout << "Left window" << std::endl;
     }
 }
 
 void mouseButtonCallback( GLFWwindow *window, int button, int action, int mods )
 {
-    if ( button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS )
-    {
-        std::cout << "Right button pressed" << std::endl;
-    }
+//    if ( button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS ) {
+//        std::cout << "Right button pressed" << std::endl;
+//    }
+//
+    // Hold and drag logic
+//     if ( button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS ) {
+//         cout << "That is the actual fuck";
+//     }
 }
 
 void scrollCallback( GLFWwindow *window, double xoffset, double yoffset )
 {
     std::cout << xoffset << " : " << yoffset << std::endl;
+    
 }
